5d43f42f2d2864295b41e1bd376caf6e0a110f73
Harry Duan <gua@desgard.com>
2018-09-02 18:02:35 +0800

    增加 Nimble 单元测试样例
    [Feature] 增加 Nimble 单元测试样例
    
    Signed-off-by: Harry Duan <gua@desgard.com>

--- Straycat/StraycatTests/StraycatKannaTrendingTests.swift
+++ Straycat/StraycatTests/StraycatKannaTrendingTests.swift
@@ -6,30 +6,62 @@
 //  Copyright © 2018 Harry Twan. All rights reserved.
 //
 
+
+import Nimble
 import XCTest
 
 @testable import Straycat
 
 class StraycatKannaTrendingTests: XCTestCase {
+
+    static let AsyncTimeout: TimeInterval = 10
+    static let ItemsCount = 25
+
+    typealias TT = StraycatKannaTrendingTests
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+
+    /// 测试获取数据条目
+    func testTrendingRepoItemsCount() {
+        waitUntil(timeout: TT.AsyncTimeout) { done in
+            StrayTrending.shared.fetchRepo(tool: .kanna) { success, repos in
+                guard let repos = repos, success else {
+                    fail("The result is Nil")
+                    return
+                }
+                expect(repos.count).to(equal(TT.ItemsCount))
+                done()
+            }
         }
     }
+
+    /// 测试获取数据
+    func testTrendingRepoItems() {
+        waitUntil(timeout: TT.AsyncTimeout) { done in
+            StrayTrending.shared.fetchRepo(tool: .kanna) { (success, repos) in
+                guard let repos = repos, success else {
+                    fail("The result is Nil")
+                    return
+                }
+                let repoName = repos.map { $0.fullname }.filter { $0 != "" }
+                expect(repoName.count).to(equal(TT.ItemsCount),
+                                          description: "The repo name avatar parser is bad!")
+                let repoStar = repos.map { $0.star }.filter { $0 != 0 }
+                expect(repoStar.count).to(equal(TT.ItemsCount),
+                                          description: "The repo stars avatar parser is bad!")
+                let repoAvatar = repos.map { $0.avatar }.filter { $0 != [] }
+                expect(repoAvatar.count).to(equal(TT.ItemsCount),
+                                            description: "The repo author avatar parser is bad!")
+                done()
+            }
+        }
+    }
+
+    /// 测试图片 URL 
 }
